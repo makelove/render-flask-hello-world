b460a48bf1f4803fbc111e5d8587d8131302901f
play4fun <play4fun@foxmail.com>
2023-11-13 23:31:44 +0800

    datetime

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Follow the guide at https://render.com/docs/deploy-flask.
 # 中文
 执行
 gunicorn app:app
+
+
+# 问题
+- render No open HTTP ports detected
+https://community.render.com/t/no-open-http-ports-detected/14511/2
+Ensure the server is listening on 0.0.0.0, and not 127.0.0.1.
--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 import requests
+from datetime import datetime
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f'Hello, World! {datetime.now()}'
 
 @app.route('/ip')
-def hello_wo2rld():
+def ip():
     url='https://httpbin.org/get'
     rs=requests.get(url)
+    print('result:',url,rs.text)
     return rs.json()
 
